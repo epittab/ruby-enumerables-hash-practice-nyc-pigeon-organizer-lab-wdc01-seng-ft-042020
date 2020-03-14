340564e23d711034c72515de0d4e306365915168
Enrique Pittaluga <ep7m@me.com>
2020-03-14 17:35:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  reorganized_hash = {}
+  
+  data.each_pair do |pair|
+  
+      
+      puts pair
+      
+    
+  end
+  puts reorganized_hash
 end
