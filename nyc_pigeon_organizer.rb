5e13733f4fa381830b14193d3f63748dde653ce9
Enrique Pittaluga <ep7m@me.com>
2020-03-14 17:38:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,17 @@
 def nyc_pigeon_organizer(data)
-  
+   
+   hash = {}
+
+    data.each do |key, value|
+
+        hash = create_new_pigeon_key(key, value, hash)
+
+        hash = add_second_level(key, hash)
+
+        hash = add_third_level_array(key, value, hash)
+
+    end
+   
+    return hash
  
 end
